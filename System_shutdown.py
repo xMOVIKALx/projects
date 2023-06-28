5e4f521cc88d433014f446f2a9f869db2b497241
xMOVIKALx <128097461+xMOVIKALx@users.noreply.github.com>
2023-06-28 21:52:18 +0330

    System_shutdown.py

--- System_shutdown.py
+++ System_shutdown.py
@@ -1,16 +1,24 @@
 import os
 def app() :
     op = input("what do you want to do?\n"
-           "1-Shutdown\n"
-           "2-restart : ")
-    if op == "1" :
+           "1-Shutdown : ")
+    time = int(input("How many more minutes? ( + I want shutdown now!) \n if you want shutdown now enter + : "))
+    if time == "+" :
+        ok = input("are you sure? (y/n) : ")
+        if ok == "y":
+            os.system("shutdown /s /t 1")
+        elif ok == "n":
+            print("ok, if you want use this program again, restart the code.")
+        else:
+            print("invalid input")
+
+    else :
+        time = time * 60
         ok = input("are you sure? (y/n) : ")
         if ok == "y" :
-            os.system("shutdown /s /t 1")
+            os.system(f"shutdown /s /t {time}")
         elif ok == "n" :
             print("ok, if you want use this program again, restart the code.")
         else :
             print("invalid input")
-    else :
-        print("invalid input")
 app()
