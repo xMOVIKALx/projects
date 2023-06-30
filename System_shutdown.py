9aace7eed88ceb1aba7a4e24208b52614f6da8c7
xMOVIKALx <128097461+xMOVIKALx@users.noreply.github.com>
2023-06-30 15:21:40 +0330

    Update System_shutdown.py

--- System_shutdown.py
+++ System_shutdown.py
@@ -1,24 +1,49 @@
 import os
 def app() :
     op = input("what do you want to do?\n"
-           "1-Shutdown : ")
-    time = int(input("How many more minutes? ( + I want shutdown now!) \n if you want shutdown now enter + : "))
-    if time == "+" :
-        ok = input("are you sure? (y/n) : ")
-        if ok == "y":
-            os.system("shutdown /s /t 1")
-        elif ok == "n":
-            print("ok, if you want use this program again, restart the code.")
-        else:
-            print("invalid input")
-
-    else :
-        time = time * 60
+               "Hibernate and sleep just for now. you can't timing for them.\n"
+           "1-Shutdown\n"
+               "2-Restrat\n"
+               "3-sleep\n"
+               "4-Hibernate : "
+               )
+    daghighe = int(input("How many more minutes? : "))
+    if op == "1" :
+        zaman = daghighe * 60
         ok = input("are you sure? (y/n) : ")
         if ok == "y" :
-            os.system(f"shutdown /s /t {time}")
+            os.system(f"shutdown /s /t {zaman}")
         elif ok == "n" :
             print("ok, if you want use this program again, restart the code.")
         else :
-            print("invalid input")
+            print("invalid input.")
+    elif op == "2" :
+        zaman = daghighe * 60
+        ok = input("are you sure? (y/n) : ")
+        if ok == "y":
+            os.system(f"shutdown /r /t {zaman}")
+        elif ok == "n":
+            print("ok, if you want use this program again, restart the code.")
+        else:
+            print("invalid input.")
+    elif op == "3" :
+        zaman = daghighe * 60
+        ok = input("are you sure? (y/n) : ")
+        if ok == "y" :
+            os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+        elif ok == "n" :
+            print("ok, if you want use this program again, restart the code.")
+        else :
+            print("invalid input.")
+    elif op == "4" :
+        zaman = daghighe * 60
+        ok = input("are you sure? (y/n) : ")
+        if ok == "y" :
+            os.system(f"shutdown /h")
+        elif ok == "n" :
+            print("ok, if you want use this program again, restart the code.")
+        else :
+            print("invalid input.")
+    else :
+        print("invalid input.")
 app()
