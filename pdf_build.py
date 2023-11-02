e055e4aeae8ad3dda9b91b2bf555dba0ce94b321
xMOVIKALx <128097461+xMOVIKALx@users.noreply.github.com>
2023-11-02 21:06:54 +0330

    pdf_build.py

--- pdf_build.py
+++ pdf_build.py
@@ -11,22 +11,33 @@ try :
                 bytes_images.append(image_file.read())
     
         pdf_data = img2pdf.convert(bytes_images)
-    
-        with open(os.path.join("D:\MOVIKAL\Desktop\PDF_Output", output_filename), "wb") as output_file:
+        # Note : becareful you write correct output path folder.
+        with open(os.path.join("<<<Your output folder path here>>>", output_filename), "wb") as output_file:
             output_file.write(pdf_data)
-    
-    folder_path = input("Enter folder pass : ")
+
+    # Your images folder path
+    folder_path = input("Enter folder path : ")
+
     nowtime = datetime.now()
     nowtime = str(nowtime)[0:19]
     nowtime = nowtime.split(":")
     nowtime = ".".join(nowtime)
     nowtime = nowtime.split()
     nowtime = " ".join(nowtime)
+
+    # If you want set date and time on the last of your file you cab enter "y". if not you can enter "n"
     timeop = input("Set Time in the last? (y/n) : ")
+    
     if timeop == "y" :
-      output_filename = input("Set OutPut name : ") + " " + nowtime + ".pdf"
+        
+        # This entry for your file path name. like : MyPDF 2023-2-11 12.13.14.pdf
+        output_filename = input("Set OutPut name : ") + " " + nowtime + ".pdf"
+        
     elif timeop == "n" :
-      output_filename = input("Set OutPut name : ") + ".pdf"
+        
+        # This entry for your file path name with out date and time. like : MyPDF.pdf
+        output_filename = input("Set OutPut name : ") + ".pdf"
+        
     else :
         print("Please Enter something valid.\n")
         input("<Press Enter to continue>")
